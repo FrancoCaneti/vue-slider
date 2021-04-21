c9a4b52668e745893278eaa0f99cd6008ab57240
FrancoCaneti <CAnetifranco95@hmail.com>
2021-04-21 23:50:22 +0200

    next prev

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
- const app = new Vue ({
-       el:'#app',
-       data:{
-           photos: [
-            "http://www.fumettologica.it/wp-content/uploads/2014/02/10addio.gif",
-           ],
-           
- });
+const app = new Vue ({
+    el:'#app',
+    data: {
+        photos: [
+            './images/image1.jpg',
+            './images/image2.jpg',
+            './images/image3.jpg',
+            './images/image4.jpg',
+        ],
+        indexPhoto:0,
+    },
+    methods: {
+        prev(){
+            this.indexPhoto -=1;
+            if(this.indexPhoto < 0) {
+                this.indexPhoto = this.photos.lenght -1
+            }
+        },
+        next(){
+            this.indexPhoto += 1;
+            if( this.indexPhoto >  (this.photos.lenght -1)) {
+                this.index = 0;
+            }
+        }
+       
+    }
+});
