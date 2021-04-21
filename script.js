a9887e24379e1a0f59005cfb1cddbd0005c093c7
FrancoCaneti <CAnetifranco95@hmail.com>
2021-04-21 15:28:56 +0200

    image

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
  const app = new Vue ({
        el:'#app',
        data:{
-
-       
-           photos:[
-            "./images/180132.jpg",
-            "./images/3d-wallpaper-custom-photo-mural-balcony-paris.jpg",
-            "./images/japan-fuji-mountain-himeji-castle-full-cherry-blossom-h.jpg",
-            "./images/Rio-de-Janeiro.jpg",
+           photos: [
+            "http://www.fumettologica.it/wp-content/uploads/2014/02/10addio.gif",
            ],
-           indexPhoto:0,
-       },
-       methods: {
-           prevPhoto() {
-               this.indexPhoto -= 1;
-
-               if(this.indexPhoto < 0) {
-                   this.indexPhoto = this.photos.lenght - 1
-               }
-           },
-           nextPhotos() {
-               this.indexPhoto += 1;
-               if( this.indexPhoto > ( this.photos.lenght -1) ) {
-                    this.indexPhoto = 0;
-               }
-           }
-       }
+           
  });
